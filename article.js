0b35ece776241ccf08522d8acbf6e3d86e130b5c
mfmunic <mfmunic@gmail.com>
2017-05-13 14:00:06 -0700

    last edition

--- article.js
+++ article.js
@@ -1,37 +1,37 @@
-
-
-
-
-
+$(document).ready(function(){
 
 $("#runSearch").on("click", searchArt)
 
-function searchArt(){
+function searchArt(event){
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	event.preventDefault();
 
-var query = $("#searchTerm")
-var bDate
-var eDate
-$("")
-query = "trump"
-bDate = 2000
-eDate = 2010
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
-url += '?' + $.param({
-  'api-key': "02bae414f0b840cfa037ba9a60bc5139",
-  'q':query ,
-  'begin_date': bDate + "0101",
-  'end_date': eDate + "0101"
-});
+	var query = $("#searchTerm").val();
+	var bDate = $("#startYear").val();
+	var eDate = $("#endYear").val();
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});//end of ajax
+	console.log(query)
+	console.log(bDate)
+	console.log(eDate)
 
-}//end of main function
+	url += '?' + $.param({
+  		'api-key': "02bae414f0b840cfa037ba9a60bc5139",
+  		'q':query ,
+  		'begin_date': bDate + "0101",
+  		'end_date': eDate + "0101"
+	});
+
+	$.ajax({
+  		url: url,
+ 	 	method: 'GET',
+	}).done(function(result) {
+  		console.log(result);
+	}).fail(function(err) {
+		console.log(err);
+  		// throw err;
+	});//end of ajax
+
+};//end of main function
+});//end of document.ready
