ef27e68b463366ee3d671ef426edfc13597ffb35
mfmunic <mfmunic@gmail.com>
2017-05-13 13:20:41 -0700

    changed

--- article.js
+++ article.js
@@ -4,11 +4,16 @@ var bDate
 var eDate
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+query = "trump"
+bDate = 2000
+eDate = 2010
+
 url += '?' + $.param({
   'api-key': "02bae414f0b840cfa037ba9a60bc5139",
-  'q':" + query + " ,
-  'begin_date': " + bDate +",
-  'end_date': " +eDate +"
+  'q':query ,
+  'begin_date': bDate + "0101",
+  'end_date': eDate + "0101"
 });
 
 $.ajax({
